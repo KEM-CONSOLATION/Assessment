b91878d61226fbc6b8009610a6ae8490effde5ab
Kem-consolation <consolationlotachi@gmail.com>
2023-03-13 13:15:53 +0100

    Concluded the authors section

--- Assessment/src/Components/Author.jsx
+++ Assessment/src/Components/Author.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-
+import AuthorsImage from '../assets/AuthorsImage.jpeg'
 const Author = () => {
   return (
     <div className=" max-w-6xl mx-auto border-t md:border-none pt-5 md:pt-0 md:mt-20">
         <div className=" mx-auto text-center md:w-2/4">
             <h1 className=" font-bold text-yellow-500 text-4xl mb-2">Our Author</h1>
             <p className=' text-xl text-slate-700'>A creative hub for over 150,000 authors <br /> We can help any author with their literary endeavor.</p>
+        </div>
+
+        <div className=" grid gap-5 sm:grid sm:gap-2 sm:mx-5 sm:grid-cols-2 md:flex md:justify-between items-center place-items-center mt-10">
+          <div className=" w-80 md:w-96 shadow-lg  px-5 py-5 bg-white">
+            <img src={AuthorsImage} alt="" srcset="" className=' py-2' />
+            <p className=' text-yellow-500 font-medium'>Jane Doe</p>
+            <p className=' font-extralight text-slate-500 py-2 mb-3'>A town hall different from bala blu, blue blu bulaba. broom broom broom brooooooooom. Bala blu blue blu bulaba. The farmers will make more money. Your lunch will not be imported, cassava garri ewa and ehhh ehhhhnn.</p>
+            <a href="#" className=' bg-slate-700 px-7 py-3 rounded text-white'>Read More</a>
+          </div>
+
+          <div className=" w-80 md:w-96 shadow-lg  px-5 py-5 bg-white">
+            <img src={AuthorsImage} alt="" srcset="" className=' py-2' />
+            <p className=' text-yellow-500 font-medium'>Jane Doe</p>
+            <p className=' font-extralight text-slate-500 py-2 mb-3'>A town hall different from bala blu, blue blu bulaba. broom broom broom brooooooooom. Bala blu blue blu bulaba. The farmers will make more money. Your lunch will not be imported, cassava garri ewa and ehhh ehhhhnn.</p>
+            <a href="#" className=' bg-slate-700 px-7 py-3 rounded text-white'>Read More</a>
+          </div>
+
+          <div className=" w-80 md:w-96 shadow-lg  px-5 py-5 bg-white">
+            <img src={AuthorsImage} alt="" srcset="" className=' py-2' />
+            <p className=' text-yellow-500 font-medium'>Jane Doe</p>
+            <p className=' font-extralight text-slate-500 py-2 mb-3'>A town hall different from bala blu, blue blu bulaba. broom broom broom brooooooooom. Bala blu blue blu bulaba. The farmers will make more money. Your lunch will not be imported, cassava garri ewa and ehhh ehhhhnn.</p>
+            <a href="#" className=' bg-slate-700 px-7 py-3 rounded text-white'>Read More</a>
+          </div>
         </div>
         
     </div>  
--- Assessment/src/Components/HeroSection.jsx
+++ Assessment/src/Components/HeroSection.jsx
@@ -3,7 +3,7 @@ const HeroSection = () => {
     <div className=" max-w-6xl mx-auto py-5 md:mx-20">
         <div className=" grid grid-cols-1 items-center place-items-center my-5 mt-10 md:flex md:items-start md:justify-between">
             <div className=" order-2 md:-order-none mt-10 w-5/6 md:w-3/6">
-                <h1 className=" text-4xl tracking-widest"><span className=" text-yellow-500 font-semibold">Writers</span>,Readers and <span className=" text-slate-300 font-extralight">Reviewers</span></h1>
+                <h1 className=" text-4xl tracking-widest"><span className=" text-yellow-500 font-medium">Writers</span>,Readers and <span className=" text-slate-300 font-extralight">Reviewers</span></h1>
                 <p className=" tracking-widest text-slate-300 text-sm mt-10">A town hall different from bala blu, blue blu bulaba. broom broom broom brooooooooom. Bala blu blue blu bulaba. The farmers will make more money. Your lunch will not be imported, cassava garri ewa and ehhh ehhhhnn. The farmer will make money, the dinner would be cassava, eba, ewa and everything.</p>
 
                 <div className=" mt-16 grid md:flex gap-5 md:gap-0 font-semibold text-center w-72 md:w-full">
@@ -12,7 +12,7 @@ const HeroSection = () => {
                 </div>
             </div>
 
-            <div className=" flex flex-col items-center justify-center mb-10 md:mb-0 h-72 w-72 mx-auto md:mx-0 md:h-96 md:w-96 rounded-full bg-green-600 relative">
+            <div className=" flex flex-col items-center justify-center mb-10 md:mb-0 h-72 w-72 mx-auto md:mx-0 md:h-96 md:w-96 rounded-full bg-yellow-600 relative">
               
             </div>
         </div>
