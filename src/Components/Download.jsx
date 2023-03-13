208e338db9c7135c9acce28b57ec2cf40b508bea
Kem-consolation <consolationlotachi@gmail.com>
2023-03-13 16:26:27 +0100

    Little adjustment on the links

--- src/Components/Download.jsx
+++ src/Components/Download.jsx
@@ -7,8 +7,8 @@ const Download = () => {
                 <h1 className=' text-2xl md:text-3xl font-medium py-3'>Download App for Exciting Deals</h1>
                 <p className=' font-normal text-sm text-slate-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In vitae error ad eligendi officia assumenda est nam aspernatur tempore tenetur.</p>
                 <div className=" flex items-center mt-5">
-                    <a href="#" className=" px-2 md:px-5 bg-white shadow-lg rounded-md py-3 mr-5 md:mr-10 text-blue-500 font-medium "> <GooglePlayFillIcon className=' inline-flex'/> Google Play</a>
-                    <a href="#" className="  px-3 md:px-5  bg-black py-3 rounded-md text-white font-medium shadow-lg"> <AppsFillIcon className=' inline-flex' />  App Store</a>
+                    <a href="#" className=" px-1 sm:px-2 md:px-5 bg-white shadow-lg rounded-md py-3 mr-5 md:mr-10 text-blue-500 font-medium "> <GooglePlayFillIcon className=' inline-flex'/> Google Play</a>
+                    <a href="#" className="px:2 sm:px-3 md:px-5  bg-black py-3 rounded-md text-white font-medium shadow-lg"> <AppsFillIcon className=' inline-flex' />  App Store</a>
                 </div>
             </div>
         </div>
